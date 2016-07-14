bab920de98880a523461a1dc16b9ce93ec10ee10
Viktor Roytman <vr2262@columbia.edu>
2016-07-14 15:12:04 -0400

    Initial commit...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # modbus-thing
+1. Enter your Python 3 environment
+2. $ pip install -r requirements.txt
---
+++ modbus.py
@@ -0,0 +1,32 @@
+import pymodbus.client.sync
+OUTLETS = {
+    1: {'coil': 300, 'amps': 202, 'watts': 222, 'kWh': 242},
+    2: {'coil': 301, 'amps': 204, 'watts': 224, 'kWh': 244},
+    3: {'coil': 302, 'amps': 206, 'watts': 226, 'kWh': 246},
+    4: {'coil': 303, 'amps': 208, 'watts': 228, 'kWh': 248},
+    5: {'coil': 304, 'amps': 210, 'watts': 230, 'kWh': 250},
+    6: {'coil': 305, 'amps': 212, 'watts': 232, 'kWh': 252},
+    7: {'coil': 306, 'amps': 214, 'watts': 234, 'kWh': 254},
+    8: {'coil': 307, 'amps': 216, 'watts': 236, 'kWh': 256},
+    9: {'coil': 308, 'amps': 218, 'watts': 238, 'kWh': 258},
+    10: {'coil': 309, 'amps': 220, 'watts': 240, 'kWh': 260}
+}
+
+pymodbus.constants.Defaults.Timeout = 0.5
+pymodbus.constants.Defaults.Retries = 1
+
+
+CLIENTS = dict()
+
+
+def get_client(ip):
+    if ip is None:
+        raise ValueError('IP address is None')
+    if ip not in CLIENTS:
+        CLIENTS[ip] = pymodbus.client.sync.ModbusTcpClient(ip)
+    return CLIENTS[ip]
+
+def read_register_bytes(client, address):
+    address -= 1
+    results = client.read_holding_registers(address=address, count=2, unit=1)
+    return results.registers
